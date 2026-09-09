430232f2da8376fd4c4971b7943e79881a6fdc2f
Adnan Mahmud Fuad <11adnan@live.com>
2026-09-09 09:56:00 +0600

    Remove debug flag from Watchtower setup
    Removed debug flag from Watchtower run command.

--- portainer-watchtower/setup_portainer_watchtower.sh
+++ portainer-watchtower/setup_portainer_watchtower.sh
@@ -16,8 +16,11 @@ log  ".........Creating Watchtower Docker Container...Step 2"
 docker run -d \
   --name watchtower \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  --log-opt max-file=7 \
+  --restart unless-stopped \
   nickfedor/watchtower \
-  --debug \
   --cleanup \
   --schedule "*/30 * * * * *"
 
